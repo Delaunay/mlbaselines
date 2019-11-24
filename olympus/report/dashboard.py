e972dc3fbfb45ccbdf742d794a30eb44078525db
Setepenre <pierre.delaunay.tr@gmail.com>
2019-11-24 18:23:09 -0500

    use track as dashboard backend (#48)

--- olympus/report/dashboard.py
+++ olympus/report/dashboard.py
@@ -5,10 +5,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 import plotly.express as px
 import plotly.graph_objs as go
 
 import pandas as pd
+
+from track.persistence import get_protocol
+from track.serialization import to_json
 
 from orion.storage.base import Storage
 from orion.core.utils import flatten
@@ -43,6 +47,9 @@ def to_html(obj, depth=0):
     elif isinstance(obj, list):
         return list_to_html(obj, depth + 1)
 
+    if depth == 0:
+        return to_html(to_json(obj), depth + 1)
+
     return obj
 
 
@@ -63,9 +70,74 @@ def list_to_html(obj, depth=0):
     return html.Ul([html.Li(to_html(i, depth + 1), className='pylist_value') for i in obj], className='pylist')
 
 
+def track_extract_data(trials, protocol):
+    from track.structure import Trial
+    metrics = []
+
+    for trial_id in trials:
+        t = Trial()
+        t.uid = trial_id
+        trial = to_json(protocol.get_trial(t)[0])
+
+        values = {'uid': trial['uid']}
+
+        for metric_name, metric_values in trial['metrics'].items():
+
+            new_metrics = {}
+            for k, v in metric_values.items():
+                new_metrics[k] = v
+
+            values[metric_name] = new_metrics
+
+        metrics.append(pd.DataFrame(values))
+
+    extracted_data = pd.concat(metrics, sort=True)
+    columns = list(extracted_data.columns)
+
+    return extracted_data, columns
+
+
+def orion_extract_data(trials):
+    results = []
+
+    for trial in trials:
+        trial_info = {'id': trial.id}
+
+        for k, v in trial.params.items():
+            if k in trial_info:
+                print(f'warning, overriding {k}')
+
+            trial_info[k] = v
+
+        for result in trial.results:
+            if result.name in trial_info:
+                print(f'warning, overriding {result.name}')
+
+            trial_info[result.name] = result.value
+
+        results.append(trial_info)
+
+    extracted_data = pd.DataFrame(results)
+    columns = list(extracted_data.columns)
+
+    return extracted_data, columns
+
+
+def orion_get_trial(uid, storage):
+    return storage.get_trial(uid=uid).to_dict()
+
+
+def track_get_trial(uid, protocol):
+    from track.structure import Trial
+    trial = Trial()
+    trial.uid = uid
+    return to_json(protocol.get_trial(trial)[0])
+
+
 class ApplicationState:
     def __init__(self):
         self.storage = None
+        self.protocol = None
         self.experiments = None
         self.experiments_names = []
         self.trials = []
@@ -74,13 +146,23 @@ class ApplicationState:
         self.extracted_data = None
         self.columns = []
         self.args = None
+        self.exp_name = None
 
-    def connect(self, storage_uri):
+    def connect(self, track_uri):
+        self.protocol = get_protocol(track_uri)
+        self.experiments = self.protocol.fetch_projects({})
+        self.experiments_names = [e.name for e in self.experiments]
+        self.experiments = {
+            e.name: to_json(e) for e in self.experiments
+        }
+
+    def connect_to_orion(self, storage_uri):
         storage_config = resolve_storage(storage_uri)
         storage_type = storage_config.pop('type')
 
         self.storage = Storage(of_type=storage_type, **storage_config)
         self.experiments = self.storage.fetch_experiments({})
+
         self.experiments_names = [e['name'] for e in self.experiments]
         self.experiments = {
             e['name']: e for e in self.experiments
@@ -88,34 +170,29 @@ class ApplicationState:
 
     def fetch_trials(self, exp_name):
         self.current_exp = exp_name
-        self.trials = self.storage.fetch_trials(uid=self.experiments[exp_name]['_id'])
+
+        if self.storage:
+            self.trials = self.storage.fetch_trials(uid=self.experiments[exp_name]['_id'])
+        else:
+            self.trials = self.experiments[exp_name]['groups'][0]['trials']
+
+        self.exp_name = exp_name
         self.extract_data()
 
     def get_trial(self, id):
-        return self.storage.get_trial(uid=id).to_dict()
+        if self.storage:
+            return orion_get_trial(id, self.storage)
+
+        return track_get_trial(id, self.protocol)
 
     def extract_data(self):
-        results = []
+        if self.storage:
+            a, b = orion_extract_data(self.trials)
+        else:
+            a, b = track_extract_data(self.trials, self.protocol)
 
-        for trial in self.trials:
-            trial_info = {'id': trial.id}
-
-            for k, v in trial.params.items():
-                if k in trial_info:
-                    print(f'warning, overriding {k}')
-
-                trial_info[k] = v
-
-            for result in trial.results:
-                if result.name in trial_info:
-                    print(f'warning, overriding {result.name}')
-
-                trial_info[result.name] = result.value
-
-            results.append(trial_info)
-
-        self.extracted_data = pd.DataFrame(results)
-        self.columns = list(self.extracted_data.columns)
+        self.extracted_data = a
+        self.columns = b
 
 
 def experiment_side_panel():
@@ -142,7 +219,7 @@ def trials_side_panel():
 
     items = dcc.RadioItems(
         id='trials_id',
-        options=[{'label': format_objective(i, t.objective), 'value': t.id} for i, t in enumerate(state.trials)],
+        options=[{'label': t[:10], 'value': t} for i, t in enumerate(state.trials)],
         value=None
     )
 
@@ -208,8 +285,11 @@ def make_dials():
 def main(args=None):
     global state
 
+    orion_uri = 'legacy:pickleddb:full_test.pkl'
+    track_uri = 'file:/home/setepenre/work/olympus-run/track_test.json'
+
     parser = ArgumentParser()
-    parser.add_argument('--storage-uri', type=str, default='legacy:pickleddb:full_test.pkl')
+    parser.add_argument('--storage-uri', type=str, default=track_uri)
 
     args = parser.parse_args(args)
 
@@ -268,8 +348,14 @@ def main(args=None):
         Output(component_id='exp-details', component_property='children'),
         [Input(component_id='experiment_name', component_property='value')])
     def get_experiment_details(exp_name):
+        import copy
+
         if exp_name is not None:
-            data = state.experiments[exp_name]
+            data = copy.deepcopy(state.experiments[exp_name])
+
+            data.pop('trials')
+            [g.pop('trials') for g in data['groups']]
+
             return [html.H4('Experiment: {}'.format(exp_name)), to_html(data)]
 
         return None
@@ -282,7 +368,6 @@ def main(args=None):
     def show_trials(exp_name):
         if exp_name is not None:
             state.fetch_trials(exp_name)
-
             return trials_side_panel(), make_dials()
 
         return None, None
@@ -304,8 +389,11 @@ def main(args=None):
 
         if trial_id is not None:
             data = state.get_trial(trial_id)
-            data['params'] = simplify(data.pop('params', []))
-            data['results'] = simplify(data.pop('results', []))
+
+            if state.storage:
+                data['params'] = simplify(data.pop('params', []))
+                data['results'] = simplify(data.pop('results', []))
+
             return [html.H4('Trial: {}'.format(trial_id)), to_html(data)]
 
         return None
@@ -316,27 +404,43 @@ def main(args=None):
          Input('yaxis-column', 'value'),
          Input('xaxis-type', 'value'),
          Input('yaxis-type', 'value')])
-    def update_graph(xaxis_col, yaxis_col, xaxis_type, yaxis_type):
+    def update_graph(xaxis_col, yaxis_col, xaxis_type, yaxis_type, group_by='uid'):
         if xaxis_col is None or yaxis_col is None:
             return {}
 
-        xaxis_type = xaxis_type.lower()
-        yaxis_type = yaxis_type.lower()
+        if group_by:
+            xaxis_type = xaxis_type.lower()
+            yaxis_type = yaxis_type.lower()
 
-        x_data = state.extracted_data[xaxis_col]
-        y_data = state.extracted_data[yaxis_col]
+            groups = state.extracted_data.groupby([group_by])
 
-        return {
-            'data': [go.Scatter(
-                x=x_data,
-                y=y_data,
+            lines = []
+            for k, _ in groups.indices.items():
+                group = groups.get_group(k)
+
+                lines.append(go.Scatter(
+                    x=group[xaxis_col],
+                    y=group[yaxis_col],
+                    mode='lines',
+                    name=k[:10]
+                ))
+        else:
+            lines = []
+            group = state.extracted_data
+
+            lines.append(go.Scatter(
+                x=group[xaxis_col],
+                y=group[yaxis_col],
                 mode='markers',
                 marker={
                     'size': 15,
                     'opacity': 0.5,
                     'line': {'width': 0.5, 'color': 'white'}
                 }
-            )],
+            ))
+
+        return {
+            'data': lines,
             'layout': go.Layout(
                 xaxis={
                     'title': prettify_name(xaxis_col),
