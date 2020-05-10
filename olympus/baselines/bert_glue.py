2c8484e12dc3d89d841efb55ed612a99d32d02a9
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-05-10 15:30:37 -0400

    Remove comments for bootstrap in bert baseline

--- olympus/baselines/bert_glue.py
+++ olympus/baselines/bert_glue.py
@@ -30,16 +30,10 @@ def main(task='rte', bootstrapping_seed=1, sampler_seed=1, init_seed=1, global_s
     base_folder = options('state.storage', '/tmp/storage')
     storage = StateStorage(folder=base_folder, time_buffer=5 * 60)
 
-    # sampling_method = {
-    #     'name': 'bootstrap', 'ratio': 1,
-    #     'split_ratio': 0.1666,
-    #     'seed': bootstraping_seed}
-
     task = classification_baseline(
         "bert-{}".format(task), 'normal', 'adam',
         schedule='warmup',
         dataset="glue-{}".format(task),
-        # sampling_method=sampling_method,
         sampling_method='original',
         sampler_seed=sampler_seed, init_seed=init_seed,
         batch_size=batch_size, device=fetch_device(),
