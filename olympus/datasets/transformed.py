3dc46c4833ee918936c3a6b77ca93256fe775640
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-05-12 08:57:06 -0400

    Remove docstrings with bad format...
    It's copied code from torchvision anyway. We should consider
    contributing back.

--- olympus/datasets/transformed.py
+++ olympus/datasets/transformed.py
@@ -29,33 +29,6 @@ class TransformedSubset(Subset):
 
 # NOTE: Copied over from torchvision. Should consider contributing to it directly.
 class RandomCrop(object):
-    """Crop the given PIL Image at a random location.
-    Args:
-        size (sequence or int): Desired output size of the crop. If size is an
-            int instead of sequence like (h, w), a square crop (size, size) is
-            made.
-        padding (int or sequence, optional): Optional padding on each border
-            of the image. Default is None, i.e no padding. If a sequence of length
-            4 is provided, it is used to pad left, top, right, bottom borders
-            respectively. If a sequence of length 2 is provided, it is used to
-            pad left/right, top/bottom borders, respectively.
-        pad_if_needed (boolean): It will pad the image if smaller than the
-            desired size to avoid raising an exception. Since cropping is done
-            after padding, the padding seems to be done at a random offset.
-        fill: Pixel fill value for constant fill. Default is 0. If a tuple of
-            length 3, it is used to fill R, G, B channels respectively.
-            This value is only used when the padding_mode is constant
-        padding_mode: Type of padding. Should be: constant, edge, reflect or symmetric. Default is constant.
-             - constant: pads with a constant value, this value is specified with fill
-             - edge: pads with the last value on the edge of the image
-             - reflect: pads with reflection of image (without repeating the last value on the edge)
-                padding [1, 2, 3, 4] with 2 elements on both sides in reflect mode
-                will result in [3, 2, 1, 2, 3, 4, 3, 2]
-             - symmetric: pads with reflection of image (repeating the last value on the edge)
-                padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
-                will result in [2, 1, 1, 2, 3, 4, 4, 3]
-    """
-
     def __init__(self, size, seed=None, padding=None, pad_if_needed=False, fill=0, padding_mode='constant'):
         if isinstance(size, numbers.Number):
             self.size = (int(size), int(size))
@@ -69,13 +42,6 @@ class RandomCrop(object):
 
     @staticmethod
     def get_params(rng, img, output_size):
-        """Get parameters for ``crop`` for a random crop.
-        Args:
-            img (PIL Image): Image to be cropped.
-            output_size (tuple): Expected output size of the crop.
-        Returns:
-            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
-        """
         w, h = _get_image_size(img)
         th, tw = output_size
         if w == tw and h == th:
@@ -86,12 +52,6 @@ class RandomCrop(object):
         return i, j, th, tw
 
     def __call__(self, img):
-        """
-        Args:
-            img (PIL Image): Image to be cropped.
-        Returns:
-            PIL Image: Cropped image.
-        """
         if self.padding is not None:
             img = F.pad(img, self.padding, self.fill, self.padding_mode)
 
@@ -118,22 +78,11 @@ class RandomCrop(object):
 
 # NOTE: Copied over from torchvision. Should consider contributing to it directly.
 class RandomHorizontalFlip(object):
-    """Horizontally flip the given PIL Image randomly with a given probability.
-    Args:
-        p (float): probability of the image being flipped. Default value is 0.5
-    """
-
     def __init__(self, p=0.5, seed=None):
         self.p = p
         self.rng = random.Random(seed)
 
     def __call__(self, img):
-        """
-        Args:
-            img (PIL Image): Image to be flipped.
-        Returns:
-            PIL Image: Randomly flipped image.
-        """
         if self.rng.random() < self.p:
             return F.hflip(img)
         return img
